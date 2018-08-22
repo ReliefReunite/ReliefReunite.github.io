fedcd9d99bcb31909aa107967b388659cff2bf9a
suraj-ranganath <suraj.ranganath@gmail.com>
2018-08-22 23:53:18 +0530

    Revert "Reverted to new design"
    This reverts commit b8c29f25a0b8a725145a06681378b7356a3a8a06.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   const hideLogout = document.getElementById('hideLogout');
   const btnReunite = document.getElementsByClassName('btnReunite');
   if(firebaseUser){
+    hideLogout.classList.remove('hide');
     const btnLogout = document.getElementById('btnLogout');
-      btnLogout.classList.remove('hide');
     btnLogout.addEventListener('click', e=> {
             swal({
             icon: "warning",
@@ -33,34 +33,4 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
       window.location="login.html";
     });}
   }
-});
-
-//Closes the navbar on click
-$(function(){
-    var navMain = $(".navbar-collapse");
-    navMain.on("click", "a:not([data-toggle])", null, function () {
-        navMain.collapse('hide');
-    });
-});
-
-//highlights the nav-items on scroll
-var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
-
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
-
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
-
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
 });
